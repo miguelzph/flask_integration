3f991142947b44912480b067564b1128fa292313
Miguel Z <miguelzph@gmail.com>
2023-05-31 17:15:36 -0300

    debug=false

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from integracao_hotmart import app
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+is_dev = os.getenv("DEBUG")
+
+debug=False
+
+if is_dev:
+    debug=True
+    
 # debug = True faz com que toda mudança do código atualize automaticamente
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=debug)
